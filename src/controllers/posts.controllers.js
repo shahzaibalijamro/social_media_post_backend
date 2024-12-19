9548e6156045f394703f8a4db2f5092fa5f2ab93
ShahzaibAliJamro <shahzaibalijamro@gmail.com>
2024-12-19 14:29:06 +0500

    like and unlike

--- src/controllers/posts.controllers.js
+++ src/controllers/posts.controllers.js
@@ -25,16 +25,17 @@ const addPost = async (req, res) => {
         if (mediaPath) {
             media = await uploadImageToCloudinary(mediaPath)
         }
-        const newPost = await Post.create({ 
-            content : content || "", 
-            poster, 
-            media })
+        const newPost = await Post.create({
+            content: content || "",
+            poster,
+            media
+        })
         const updateUser = await User.findByIdAndUpdate(poster, {
             $push: { posts: newPost._id }
         })
         res.status(201).json({
             message: "Post uploaded!",
-            post : newPost
+            post: newPost
         })
     } catch (error) {
         res.status(500).json({
@@ -44,7 +45,7 @@ const addPost = async (req, res) => {
     }
 }
 
-const allPosts = async (req,res) => {
+const allPosts = async (req, res) => {
     try {
         const allPosts = await Post.find({})
         res.json(allPosts)
@@ -53,15 +54,27 @@ const allPosts = async (req,res) => {
     }
 }
 
-const likePost = async (req,res) => {
+const likePost = async (req, res) => {
     try {
-        const {post,liker} = req.body;
+        const { post, liker } = req.body;
         // Validate input
         if (!post || !mongoose.Types.ObjectId.isValid(post)) {
             return res.status(400).json({ message: "Post ID is required and must be valid." });
         }
         if (!liker || !mongoose.Types.ObjectId.isValid(liker)) {
             return res.status(400).json({ message: "Liker ID is required and must be valid." });
+        }
+        //check if the post is Liked Already
+        const checkIfLikedAlready = await Like.findOne({post: post,liker: liker});
+        //if the post has already been liked
+        if (checkIfLikedAlready) {
+            //unlike the post
+            const removeLikeFromPost = await Post.findByIdAndUpdate(post, { $pull: { likes: checkIfLikedAlready._id } })
+            const removeLikedPostFromUser = await User.findByIdAndUpdate(liker, { $pull: { likedPosts: checkIfLikedAlready._id } })
+            const deleteLikedDocument = await Like.findByIdAndDelete(checkIfLikedAlready._id)
+            return res.status(200).json({
+                message : "Unliked the post!"
+            })
         }
         // Check if post exists
         const doesPostExist = await Post.findById(post);
@@ -73,25 +86,38 @@ const likePost = async (req,res) => {
         if (!doesUserExist) {
             return res.status(404).json({ message: "User doesn't exist!" });
         }
-        const session = await mongoose.startSession();
-        session.startTransaction()
-        try {
-            const like = await Like.create([{ post, liker }], { session });
-            
-            // const saveLike = await Post.findByIdAndUpdate(post, {$push: {likes: like[0]._id}},{ session })
-            // const updateUserLikedPosts = await User.findByIdAndUpdate(liker, {$push: {likedPosts: {post,liker}}},{ session })
-            session.commitTransaction();
-            res.status(200).json({
-                message: "Post liked!"
-            })
-        } catch (error) {
-            session.abortTransaction()
-            return res.status(400).json(error)
-        } finally {
-            session.endSession();
-        }
-    } catch (error) {
-        res.json(error)
+        //create like document
+        const like = await Like.create({ post, liker })
+        if (!like) return res.status(400).json({
+            error: "error"
+        })
+        //update the post's likes
+        const updatePostLikes = await Post.findByIdAndUpdate(post, { $push: { likes: like._id }},{ new: true })
+    //if failed delete the original like document
+    if (!updatePostLikes) {
+        const deleteLike = await Like.findByIdAndDelete(like._id);
+        return res.status(400).json({
+            error: "error occured first"
+        })
     }
+    //update User's liked posts
+    const updateUserLikedPosts = await User.findByIdAndUpdate(liker, { $push: { likedPosts: like._id } });
+    //if failed undo the previous 2 operations
+    if (!updateUserLikedPosts) {
+        const deleteLike = await Like.findByIdAndDelete(like._id);
+        const deleteLikeFromPost = await Post.findByIdAndUpdate(post, { $pull: { likes: like._id } })
+        return res.status(400).json({
+            error: "error occured second"
+        })
+    }
+    res.status(200).json({
+        updatePostLikes,
+        message: "Post liked successfully"
+    });
+} catch (error) {
+    res.status(400).json(error)
 }
-export { addPost, allPosts , likePost}
+}
+// const updatePostLikes = await Post.findByIdAndUpdate(post, {$push: {likes: like[0]._id}},{ session })
+// const updateUserLikedPosts = await User.findByIdAndUpdate(liker, {$push: {likedPosts: {post,liker}}},{ session })
+export { addPost, allPosts, likePost }
